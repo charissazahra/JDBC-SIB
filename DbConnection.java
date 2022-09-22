29c163657626ec51ff56d4f316057a1a33c5f5bc
charissazahra <80141662+charissazahra@users.noreply.github.com>
2022-09-22 23:33:35 +0700

    Update and rename .gitignore to DbConnection.java

---
+++ DbConnection.java
@@ -0,0 +1,31 @@
+//ini menyambungkan koneksi java ke db localhost.
+//library dan di bagian services tuh database nya connect dulu.
+
+//Bagian Database, berisi kelas yang bertugas untuk membuka koneksi dengan database tertentu dan
+//memberikan objek connection ke kelas dao yang membutuhkan koneksi.
+
+package tools;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Admin
+ */
+public class DbConnection {
+    private Connection con;
+   
+    public Connection getConnection()
+    { try {
+            
+    
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        this.con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db?zeroDateTimeBehavior=CONVERT_TO_NULL", "root", "");
+        
+    } catch (Exception e) {
+            System.out.println("Erorr + "+ e.getMessage ());
+}
+    return con;
+    } 
+}
